3d36435b03f89ccbea819f4f8cf6eba94354d519
skener <skenerster@gmail.com>
2017-12-07 20:21:56 +0200

    int1

---
+++ index.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title> Andriy Tserkovnyk </title>
+
+    <! -- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <! -- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900"
+          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
+          rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+    <! -- Custom styles for this template -->
+    <link href="css/resume.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none"> Andriy Tserkovnyk </span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile1.png" alt="Andriy Tserkovnyk">
+        </span>
+    </a>
+    <button class="navbar-toggler" type="button" data - toggle="collapse" data - target="#navbarSupportedContent" aria -
+            controls="navbarSupportedContent" aria - expanded="false" aria - label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#about"> About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#experience"> Experience</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#education"> Education</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#skills"> Skills</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#interests"> Interests</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container-fluid p-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+            <h1 class="mb-0"> Andriy
+                <span class="text-primary"> Tserkovnyk</span>
+            </h1>
+            <div class="subheading mb-5"> Lviv · Ukraine ( +380 ) 985 - 6585
+                <p><a href="mailto:skenerster@gmail.com"> skenerster@gmail.com </a></p>
+            </div>
+            <p class="mb-5">
+            <p>• Disciplined / productive / punctual / sociable / friendly / learning quickly</p>
+            <p>• Do not smoke / support a healthy lifestyle, cycling</p>
+            </p>
+            <ul class="list-inline list-social-icons mb-0">
+                <li class="list-inline-item">
+                    <a href="http://facebook.com/skenerster">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                </span>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="http://twitter.com/skener1979">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="http://linkedin.com/in/andrewtserkovnyk/">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                </span>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="http://github.com/skener">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+            <h2 class="mb-5"> Experience</h2>
+
+            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                <div class="resume-content mr-auto">
+                    <h3 class="mb-0"> Web PHP Developer </h3>
+                    <div class="subheading mb-3"> Single page HTML</div>
+                    <p><a href="http://zanzibar-servis.com.ua"> zanzibar-servis.com.ua</a></p>
+                    <p><a href="http://kostum.net.ua"> kostum.net.ua </a></p>
+                </div>
+                <div class="resume-date text-md-right">
+                    <span class="text-primary">  2013 - 2014 </span>
+                </div>
+            </div>
+
+            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                <div class="resume-content mr-auto">
+                    <h3 class="mb-0"> Web Developer </h3>
+                    <div class="subheading mb-3"> Karaoke song search app</div>
+                    <p> http://marakesh.com </p>
+                </div>
+                <div class="resume-date text-md-right">
+                    <span class="text-primary"> 2015 </span>
+                </div>
+            </div>
+
+            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                <div class="resume-content mr-auto">
+                    <h3 class="mb-0"> Parser Assistant </h3>
+                    <div class="subheading mb-3"> Dobrokar AutoSale</div>
+                    <p><a href="http://dobrokar.com">dobrokar.com</a></p>
+                </div>
+                <div class="resume-date text-md-right">
+                    <span class="text-primary"> 2016 </span>
+                </div>
+            </div>
+
+            <div class="resume-item d-flex flex-column flex-md-row">
+                <div class="resume-content mr-auto">
+                    <h3 class="mb-0"> Laravel Back-End Web-shop </h3>
+                    <div class="subheading mb-3"> WebShop</div>
+                    <p> Admin DashBoard</p>
+                </div>
+                <div class="resume-date text-md-right">
+                    <span class="text-primary"> 2017 </span>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+        <div class="my-auto">
+            <h2 class="mb-5"> Education</h2>
+
+            <div class="resume-item d-flex flex-column flex-md-row mb-5">
+                <div class="resume-content mr-auto">
+                    <h3 class="mb-0"> Lviv University of Ivan Franko </h3>
+                    <div class="subheading mb-3"> Bachelor of Geography</div>
+                </div>
+                <div class="resume-date text-md-right">
+                    <span class="text-primary"> September 1997 - June 2002 </span>
+                </div>
+            </div>
+
+            <div class="resume-item d-flex flex-column flex-md-row">
+                <div class="resume-content mr-auto">
+                    <h3 class="mb-0"> Lviv ITea Academy </h3>
+                    <div class="subheading mb-3"> PHP ADVANCE COURSE</div>
+                </div>
+                <div class="resume-date text-md-right">
+                    <span class="text-primary"> 2017 </span>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+        <div class="my-auto">
+            <h2 class="mb-5"> Skills</h2>
+
+            <div class="subheading mb-3"> Programming Languages & Tools</div>
+            <ul class="list-inline list-icons">
+                <li class="list-inline-item">
+                    <i class="devicons devicons-php"></i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="devicons devicons-laravel"></i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="devicons devicons-mysql"></i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="devicons devicons-html5"></i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="devicons devicons-css3"></i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="devicons devicons-jquery"></i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="devicons devicons-git"></i>
+                </li>
+                <li class="list-inline-item">
+                    <i class="devicons devicons-bootstrap"></i>
+            </ul>
+
+            <div class="subheading mb-3"> Workflow</div>
+            <ul class="fa-ul mb-0">
+                <li>
+                    <i class="fa-li fa fa-check"></i>
+                    Html& Css - Mobile - First, Responsive Design
+                </li>
+                <li>
+                    <i class="fa-li fa fa-check"></i>
+                    OOP Approach
+                </li>
+                <li>
+                    <i class="fa-li fa fa-check"></i>
+                    Optimization & Security
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <div class="my-auto">
+            <h2 class="mb-5"> Interests</h2>
+            <p> Healthy lifestyle, cycling</p>
+            <p> Сonstant desire to understand the laws of nature, the universe, the cosmos</p>
+            <p>I love nature and traveling.</p>
+        </div>
+    </section>
+</div>
+</section>
+
+</div>
+
+<! -- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<! -- Plugin JavaScript -->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<! -- Custom scripts for this template -->
+<script src="js/resume.min.js"></script>
+
+</body>
+
+</html>
